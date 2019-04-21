1357d17a3c54e931dfb6e67bb73d6cb47500d77a
xiegangd <xiegangd@gmail.com>
2019-04-21 23:03:16 +0800

    minimizing css for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: false } }],
+            },
+            canPrint: true
+        })
+    ]
 });
