bbbaac357398d15ef7f8e9e7d386f5e8cbb570b3
xiegangd <xiegangd@gmail.com>
2019-04-24 23:08:30 +0800

    babel-loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
-console.log('hello webpack');
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet(){
+        console.log(`hello ${this.name}`);
+    }
+}
+
+let user = new User('webpack');
+user.greet();
 
 import '../scss/index.scss';
